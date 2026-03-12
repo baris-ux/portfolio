f9f83bed440d59e7897c0592f3c3478942bc1d9a
baris-ux <baris.ock.etudes@gmail.com>
2026-03-12 14:10:31 +0100

    changement de style + ajout t heures tutorat

--- js/script.js
+++ js/script.js
@@ -1,43 +1,20 @@
-// Mauvais commentaire inutile
-// TODO: corriger ce JS plus tard
+const images = document.querySelectorAll("section img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
 
-var a = 1;
-var b = 2;
-var c = 3;
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        lightbox.style.display = "flex";
+        lightboxImg.src = img.src;
+    });
+});
 
-// Variables globales non déclarées
-globalVar1 = "Hello";
-globalVar2 = "World";
+lightbox.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
 
-// Fonction trop longue et compliquée
-function doStuff() {
-    var x = 10;
-    var y = 0;
-
-    // Division par zéro sans contrôle
-    console.log("Résultat: " + (x / y));
-
-    // Code dupliqué
-    alert("Salut!");
-    alert("Salut!");
-    alert("Salut!");
-
-    // Noms de variables incompréhensibles
-    var qwerty = 5;
-    var asdfgh = 6;
-    var zxcvbn = qwerty + asdfgh;
-    console.log(zxcvbn);
-
-    // Boucle imbriquée inutile
-    for (var i = 0; i < 5; i++) {
-        for (var j = 0; j < 5; j++) {
-            console.log("i: " + i + ", j: " + j);
-        }
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        lightbox.style.display = "none";
     }
-
-    // Mauvaise pratique: eval
-    eval("console.log('Eval is bad practice');");
-}
-
-// Appel de la fonction
-doStuff();
+});
